2408319a1e7bb4ae1a7ee5e2cb34f981d96ac476
123Dome <dominik.puck@web.de>
2023-05-13 10:15:30 +0200

    test

--- src/Prog2/Übung4/Euler.java
+++ src/Prog2/Übung4/Euler.java
@@ -2,12 +2,7 @@ package Prog2.Übung4;
 
 public class Euler {
     public static int berechne(int range) {
-        int res = 0;
-        for (int i = range; true; i += range) {
-            for(int j = 1; i <= range; i++){
-                if(j % i != 0) break;
-            }
-            return res = i;
-        }
+        int i = range;
+        return 0; //TODO
     }
 }
